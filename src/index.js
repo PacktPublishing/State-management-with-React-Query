cbfe7bf5308318cd73ce070c73d3f3334846c679
Daniel Afonso <danieljcafonso@gmail.com>
2022-11-17 11:18:33 +0000

    feat: add toogle devtools production

--- src/index.js
+++ src/index.js
@@ -7,14 +7,50 @@ import GlobalProvider from "./GlobalContext/GlobalContext";
 import myCounter from "./Mobx/Counter";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+// Use if your bundler doesn't support package exports
+// const ReactQueryDevtoolsProduction = React.lazy(() =>
+//   import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(
+//     (d) => ({
+//       default: d.ReactQueryDevtools,
+//     }),
+//   ),
+// )
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/production").then((d) => ({
+    default: d.ReactQueryDevtools,
+  }))
+);
+
+const CombinedProviders = () => {
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    window.toggleDevtools = () =>
+      setShowDevtools((previousState) => !previousState);
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <CountProvider>
+          <GlobalProvider>
+            <App counter={myCounter} />
+          </GlobalProvider>
+        </CountProvider>
+      </Provider>
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
+    </QueryClientProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <CountProvider>
-    <GlobalProvider>
-      <App counter={myCounter} />
-    </GlobalProvider>
-    </CountProvider>
-  </Provider>
-);
+root.render(<CombinedProviders />);
