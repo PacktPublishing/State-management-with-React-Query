5b234b97ceded4219da2f4644999c5cc506b8fb6
Daniel Afonso <danieljcafonso@gmail.com>
2022-11-28 09:25:13 +0000

    feat: add apply what you learned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-const fetchAPI = async () => {
-  const { data } = await axios.get(
-    `https://danieljcafonso.builtwithdark.com/react-query-api`
+const fetchData = async ({ queryKey }) => {
+  const { apiName } = queryKey[0];
+  const response = await fetch(
+    `https://danieljcafonso.builtwithdark.com/${apiName}`
   );
-  return data;
+  if (!response.ok) throw new Error("Something failed in your request");
+  return response.json();
 };
 
-const App = () => {
-  const { data } = useQuery({
-    queryKey: ["api"],
-    queryFn: fetchAPI,
+const apiA = "react-query-api";
+const apiB = "react-query-api-two";
+
+const ComponentA = () => {
+  const { data, error, isLoading, isError, isFetching } = useQuery({
+    queryKey: [{ queryIdentifier: "api", apiName: apiA }],
+    queryFn: fetchData,
+    retry: 1,
   });
 
+  if (isLoading) return <div> Loading data... </div>;
+
+  if (isError)
+    return (
+      <div> Something went wrong... Here is the error: {error.message}</div>
+    );
+
   return (
-    <div className="App">
-       {data ? data.hello : "Loading..."}
+    <div>
+      <p>{isFetching ? "Fetching Component A..." : data.hello} </p>
+      <ComponentB/>
     </div>
   );
 };
 
-export default App;
+const ComponentB = () => {
+  const { data } = useQuery({
+    queryKey: [{ queryIdentifier: "api", apiName: apiB }],
+    queryFn: fetchData,
+    onSuccess: (data) => console.log("Component B fetched data", data),
+  });
+  return (
+    <div>
+      <span>{data?.hello}</span>
+      <ComponentC parentData={data} />
+    </div>
+  );
+};
+
+const ComponentC = ({ parentData }) => {
+  const { data, isFetching } = useQuery({
+    queryKey: [{ queryIdentifier: "api", apiName: apiA }],
+    queryFn: fetchData,
+    enabled: parentData !== undefined,
+  });
+  const queryClient = useQueryClient();
+
+  return (
+    <div>
+      <p>{isFetching ? "Fetching Component C..." : data.hello} </p>
+      <button
+        onClick={() =>
+          queryClient.refetchQueries({
+            queryKey: [{ queryIdentifier: "api", apiName: apiA }],
+          })
+        }
+      >
+        Refetch Parent Data
+      </button>
+    </div>
+  );
+};
+
+export default ComponentA;
